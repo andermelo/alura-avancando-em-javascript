d112d6b91809fd1953f570e3fa7caa47e8930f3c
Anderson Melo <anderson.90@gmail.com>
2019-03-16 15:44:40 -0300

    updates da Aula 2

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -1,9 +1,27 @@
 class Negociacao {
-    constructor(){
+    constructor(data, quantidade, valor){
 
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0;
-        
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
     }
+
+    get volume(){
+
+        return this._quantidade * this._valor;
+    }
+
+    get data(){
+        return new Date(this._data.getTime());
+    }
+    
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor(){
+        return this._valor;
+    }
+
 }
