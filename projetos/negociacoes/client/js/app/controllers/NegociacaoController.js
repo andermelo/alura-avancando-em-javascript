126c731bedce518946edc872ee790356bfee67e7
andermelo <anderson.90@gmail.com>
2019-04-21 03:07:06 -0300

    inicio do curso 2

--- projetos/negociacoes/client/js/app/controllers/NegociacaoController.js
+++ projetos/negociacoes/client/js/app/controllers/NegociacaoController.js
@@ -5,7 +5,9 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+
+        this._listaNegociacoes = new ListaNegociacoes(model =>
+            this._negociacoesView.update(model));        
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -16,10 +18,16 @@ class NegociacaoController {
 
     }
 
+    apaga(){
+        this._listaNegociacoes.esvazia();
+
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
+    }
+
     adiciona(event){
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociacação adicionada com sucesso!';
         this._mensagemView.update(this._mensagem);
